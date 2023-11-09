cbb58924812fb2297e25151b473ca63580ea72ba
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-09 11:03:52 +0100

    refactored header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { LoginForm } from "./components/LoginForm";
 
 function App() {
   return (
     <>
       <Header />
+      <LoginForm />
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,3 @@
 export const Header = () => {
-  return (
-    <header>
-      <h2>Hello I'm a header</h2>
-    </header>
-  );
+  return <h1 className="text-3xl mb-3 text-slate-800">Running Site</h1>;
 };
