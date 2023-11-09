89a3fc2d8a0178214617d772aaefe22dd2e2e93a
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-09 10:25:19 +0100

    header component in

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Header } from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1 className="text-5xl text-red-500">Hello World</h1>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header>
+      <h2>Hello I'm a header</h2>
+    </header>
+  );
+};
